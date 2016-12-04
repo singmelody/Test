4e7ef453225d06ddc6872ba83985c75006f26e86
singmelody <446538645@qq.com>
2016-12-04 15:44:50 +0800

    modify

--- Test/NetReactor.cpp
+++ Test/NetReactor.cpp
@@ -10,12 +10,3 @@ NetReactor::NetReactor(void)
 NetReactor::~NetReactor(void)
 {
 }
-
-bool NetReactor::Open(uint32 nMaxNumOfHandler, uint32 tickInter /*= 1000*/)
-{
-}
-
-void NetReactor::Close()
-{
-
-}
